8d7cb9b22568991e32bd97c18691636b90418045
dylan <dylan_zeng92@qq.com>
2021-06-23 13:16:37 +0800

    export generate enums

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -19,9 +19,9 @@ test('generate enums', async () => {
   const enums = {
     CheckPeriod: { YEAR: '年度考核', QUARTER: '季度考核' },
     ResourceEnum: {
-      'center:pricing:add': '个人中心_客户守价_申请守价组',
-      'center:pricing:apply:edit': '个人中心_客户守价_申请修改',
-      'center:pricing:delete': '个人中心_客户守价_删除',
+      'center:pricing:add': '个人中心_价格_新增',
+      'center:pricing:apply:edit': '个人中心_价格_申请_修改',
+      'center:pricing:delete': '个人中心_价格_删除',
     },
   };
   await generateEnums({
